9a812c1262c9167840c4d6a38e67b6be86169030
Hyun jin <blackduvet52@gmail.com>
2023-12-25 02:51:17 +0900

    BoardController model를 이용해서 /board/list 주소에서 리스트를 뽑음

--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -5,6 +5,8 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -13,5 +15,10 @@ public class BoardService {
     public void write(Board board) {
 
         boardRepository.save(board);
+    }
+    public List<Board> boardList() {
+//      findAll() -> List를 그대로 반환해준다.
+        return boardRepository.findAll();
+
     }
 }
